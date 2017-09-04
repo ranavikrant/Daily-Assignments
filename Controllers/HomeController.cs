b78824d2dcd71cb6d128df34735c2be766cd2e75
ranavikrant <vickrantchouhan@yahoo.in>
2017-09-04 17:19:19 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Authentication_Authorization.Models;
+
+namespace Authentication_Authorization.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+     
+        public ActionResult Contact(User user)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("Login", user);
+            }
+            user.l.Add(user);
+            ViewBag.Message = "Your contact page.";
+            //User u = new User();
+           // if(ModelState.IsValid)
+            return View("Loggedin",user);
+            
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Authentication_Authorization.Models
+{
+    public class User
+    {   [Required]
+        public string username { get; set; }
+        [Required]
+        
+        public string password { get; set; }
+        public List<User> l = new List<User>();
+
+        
+    }
+}
