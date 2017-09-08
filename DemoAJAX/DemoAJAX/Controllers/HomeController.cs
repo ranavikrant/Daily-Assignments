ccf2366a31600267223180170bdff214928869ac
ranavikrant <vickrantchouhan@yahoo.in>
2017-09-08 16:47:07 +0530

    AJAX 8th sept

---
+++ DemoAJAX/DemoAJAX/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using DemoAJAX.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoAJAX.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            var population = GetProducts();
+            return Json(population, JsonRequestBehavior.AllowGet);
+           
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+           // getProducts();
+            return View("getProducts",getProducts());
+        }
+
+        private List<Products> GetProducts()
+        {
+            var ProdList = new List<Products>
+            {
+                new Products
+                {
+                    PID = "1",
+                   pName = "Chinese saman",
+
+                },
+                new Products
+                {
+                     PID  =" 2",
+                    pName = "Indaia",
+
+                },
+                new Products
+                {
+                    PID = "3",
+                   pName = "Chinese saman",
+                },
+                new Products
+                {
+                    PID = "6",
+                   pName = "Chinese saman",
+                }
+            };
+
+            return ProdList;
+
+        }
+        public ActionResult getProducts()
+        {
+            var population = GetProducts();
+            return Json(population, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ DemoAJAX/DemoAJAX/Models/Products.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoAJAX.Models
+{
+    public class Products
+    {
+        public string PID { get; set; }
+        public string pName { get; set; }
+    }
+}
